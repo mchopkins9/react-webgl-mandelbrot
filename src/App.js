e98e47ba43ff71291c9396fe6dbb2b26585c4ade
Morgan Hopkins <mchopkins9@gmail.com>
2018-04-13 02:44:37 -0400

    working on base functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,111 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Complex from 'complex';
 import './App.css';
 
+function calculateNewViewport(oldPosition,
+  oldHeight, oldAspectRatio, newAspectRatio) {
+    if (oldAspectRatio === newAspectRatio) {
+      return {position: oldPosition, height: oldHeight};
+    } else if (oldAspectRatio < newAspectRatio) {
+      const newPosition = oldPosition - Complex(
+        height * (newAspectRatio -  oldAspectRatio) / 2.0, 0);
+      
+      return {position: newPosition, height: oldHeight};
+    } else {
+      const heightDiff = height * (oldAspectRatio / newAspectRatio - 1)
+      const newPosition = oldPosition + Complex(0, heightDiff / 2.0);
+      const newHeight = oldHeight + heightDiff;
+
+      return {position: newPosition, height: newHeight}
+    }
+}
+
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      canvasHeight: undefined,
+      aspectRatio: 3.0 / 2.0,
+      viewport: {
+        position: Complex(-2, 1),
+        height: 2
+      }
+    }
+  }
+
+  handleWindowResize = () => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const newAspectRatio = width / height;
+
+    this.setState((prevState) => {
+      return {
+        canvasHeight: height,
+        aspectRatio: newAspectRatio,
+        viewport: calculateNewViewport(prevState.viewport.position,
+          prevState.viewport.height, prevState.aspectRatio, newAspectRatio)
+      }
+    })
+  }
+
+  componentDidMount() {
+    this.handleWindowResize()
+    window.addEventListener("resize", this.handleWindowResize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleWindowResize)
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div class="app">
+        <MandelbrotCanvas
+          height={this.state.canvasHeight}
+          aspectRatio={this.state.aspectRatio}
+          viewport={this.state.viewport} />
+        <ControlPanel />
       </div>
     );
   }
+
+}
+
+class MandelbrotCanvas extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.renderer = undefined;
+    this.canvasRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.renderer = MandelbrotRenderer(this.canvasRef.current); // FIXME
+  }
+
+  render() {
+    return (
+      <canvas ref={this.canvasRef} class="mandelbrot-canvas" />
+    )
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.renderer.
+  }
+
+}
+
+class ControlPanel extends Component {
+
+  render() {
+    return (
+      <div class="control-panel">Control Panel</div>
+    )
+  }
+
 }
 
 export default App;
---
+++ src/MandelbrotRenderer.js
@@ -0,0 +1,3 @@
+class MandelbrotRenderer {
+    
+}
